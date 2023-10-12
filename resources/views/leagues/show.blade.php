01d7e1d815dd2f2d8e45c202a6aa8edcea32c3bf
Fady Zaafrane <fady.zaafrane@gmail.com>
2023-10-13 00:57:55 +0100

    fifth commit

--- resources/views/leagues/show.blade.php
+++ resources/views/leagues/show.blade.php
@@ -6,7 +6,21 @@
         <div class="bg-white flex-col rounded-3xl p-8">
             <div class="flex justify-center mb-10"> <span class="text-2xl text-center font-bold">League Info</span></div>
             <div class="grid grid-cols-2 ">
-                <div class="flex justify-center border-t-2 "> <span class="mt-2 block text-gray-400 text-md-center self-center">Founded at  <time>{{$league->founded_at}}</time></span></div>
+                <div class="flex-col space-y-8 border-t-2 py-4">
+                    <div class="flex justify-center">
+                        <span class="mt-2 block font-bold text-xl">
+                            Founded at
+                            <time>{{$league->founded_at}}</time>
+                        </span>
+                    </div>
+
+                    <div class="flex-col items-center self-center">
+                        <div class="flex justify-center"><img src="/card-logos/teamLogo.png" width="100" height="100" alt="" title=""></div>
+                        <div class="flex justify-center my-2"><span class="text-md-center font-bold"><p>{{$league->teams->count()}}</p></span></div>
+                        <div><span class="text-center text-gray-400"><p>Number of teams</p></span></div>
+                    </div>
+
+                </div>
 
                 <div class="p-8 border-l-2 border-t-2 border-gray-300">
                     <div class="mb-2 self-center">
